3a519884a5b3a7f89b3ca7b9b08ceedf903fbfa6
MaximBaklanov <107793174+MaximBaklanov@users.noreply.github.com>
2025-06-30 08:02:56 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,20 +1,13 @@
-import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+import time
 
 
-def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="ru",
-                     help="Choose language: ru, en, es, etc.")
+def test_add_to_cart_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(30)
 
-
-@pytest.fixture(scope="function")
-def browser(request):
-    language = request.config.getoption("language")
-
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': language})
-
-    browser = webdriver.Chrome(options=options)
-    yield browser
-    browser.quit()
+    button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert button is not None
